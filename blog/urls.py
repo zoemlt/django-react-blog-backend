8e861cf3012fecd65c25ef7d93c49e8cffbe8b34
zoe <zoejniu@gmail.com>
2022-01-27 19:26:55 +1300

    Set up apps' urls to link with template

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-urlpatterns = [path("", TemplateView.as_view(template_name="blog/index.html"))]
+app_name = "blog"
+
+urlpatterns = [
+    path("", TemplateView.as_view(template_name="blog/index.html")),
+]
